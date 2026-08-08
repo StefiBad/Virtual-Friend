0b6c9c24d9d7a89ac8b780211345f5f8e7cf75c0
StefiBad <stefibadescu12@gmail.com>
2026-08-08 17:19:16 +0200

    Code optimization

--- gtn_game.py
+++ gtn_game.py
@@ -1,60 +1,72 @@
 import random
 
-print("Welcome to Guess the number! Let's get started!")
-play = True
-while play == True:
-  print("Select your level of difficulty (1-3)")
-  print("1. Easy (15 tries)")
-  print("2. Medium (10 tries)")
-  print("3. Hard (5 tries)")
-  print("")
-  asklev = False
-  level = int(input("And your level is: "))
-  while asklev == False:
-    if level == 1:
-      tries = 15
-      asklev = True
-    elif level == 2:
-      tries = 10
-      asklev = True
-    elif level == 3:
-      tries = 5
-      asklev = True
-    else:
-      print("This level doesn't exist!")
-      level = int(input("So your level is: "))
-  computerNum = random.randint(1, 100)
-  while tries > 0:
-    playerNum = int(input("Please, choose a number from 1 to 100: "))
-    if playerNum <= 100 and playerNum > computerNum:
-      print("Your number is higher than the number to guess ;D")
-      tries -= 1
-      print("Remaining tries:", tries)
-    elif playerNum >= 1 and playerNum < computerNum:
-      print("Your number is lower than the number to guess ;D")
-      tries -= 1
-      print("Remaining tries:", tries)
-    elif playerNum == computerNum:
-      print("Congratulations! You won the game!!!")
-      tries = -1
-    else:
-      print("You have inserted an invalid number. Try again!")
-      print("Remaining tries:", tries)
-  if tries == 0:
-    print("Oh, no! You lose the game :(")
-  askgame = False
-  while askgame == False:
-    print("Would you like to play again?")
-    print("1. Yes")
-    print("2. No")
-    print("")
-    answer = int(input("Please, answer (1-2): "))
-    if answer == 1:
-      print("Nice! Let's start the next round...")
-      askgame = True
-    elif answer == 2:
-      print("No problem! Thanks for playing with me!")
-      askgame = True
-      play = False
-    else:
-      print("Invalid option!")
+
+def play():
+    print("Welcome to Guess the number! Let's get started!\n")
+    levels = {1: 15, 2: 10, 3: 5}
+
+    while True:
+        print("Select your level of difficulty (1-3)")
+        print("1. Easy (15 tries)")
+        print("2. Medium (10 tries)")
+        print("3. Hard (5 tries)\n")
+
+        while True:
+            try:
+                level = int(input("And your level is: "))
+                if level in levels:
+                    tries = levels[level]
+                    break
+                print("This level doesn't exist! Please select 1, 2, or 3.")
+            except ValueError:
+                print("Invalid input! Please enter a number (1, 2, or 3).")
+
+        computerNum = random.randint(1, 100)
+
+        while tries > 0:
+            try:
+                playerNum = int(input("\nPlease, choose a number from 1 to 100: "))
+                if not (1 <= playerNum <= 100):
+                    print("You have inserted an invalid number. Try again!")
+                    print("Remaining tries:", tries)
+                    continue
+            except ValueError:
+                print("Invalid input! Please enter an integer from 1 to 100.")
+                print("Remaining tries:", tries)
+                continue
+
+            if playerNum > computerNum:
+                print("Your number is higher than the number to guess ;D")
+                tries -= 1
+                print("Remaining tries:", tries)
+            elif playerNum < computerNum:
+                print("Your number is lower than the number to guess ;D")
+                tries -= 1
+                print("Remaining tries:", tries)
+            else:
+                print("Congratulations! You won the game!!!")
+                break
+
+        if tries == 0:
+            print("\nOh, no! You lose the game :(")
+            print("The number to guess was:", computerNum)
+
+        print("\nWould you like to play again?")
+        print("1. Yes\n2. No\n")
+
+        while True:
+            try:
+                answer = int(input("Please, answer (1-2): "))
+                if answer == 1:
+                    print("\nNice! Let's start the next round...\n")
+                    break
+                elif answer == 2:
+                    print("\nNo problem! Thanks for playing with me!")
+                    return
+                else:
+                    print("Invalid option! Please select 1 or 2.")
+            except ValueError:
+                print("Invalid input! Please enter 1 or 2.")
+
+
+play()
